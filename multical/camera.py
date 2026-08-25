b14d3e8b77c788611934e15e1a96743616608e3e
GOD of Rap <hjjeong@neubility.co.kr>
2026-08-25 16:30:44 +0900

    [fix] tilted model is extended version of thin_prism

--- multical/camera.py
+++ multical/camera.py
@@ -43,8 +43,8 @@ class Camera(Parameters):
   model = struct(
       standard=0,
       rational=cv2.CALIB_RATIONAL_MODEL,
-      tilted=cv2.CALIB_RATIONAL_MODEL | cv2.CALIB_TILTED_MODEL,
-      thin_prism=cv2.CALIB_RATIONAL_MODEL | cv2.CALIB_TILTED_MODEL | cv2.CALIB_THIN_PRISM_MODEL,
+      thin_prism=cv2.CALIB_RATIONAL_MODEL | cv2.CALIB_THIN_PRISM_MODEL,
+      tilted=cv2.CALIB_RATIONAL_MODEL | cv2.CALIB_THIN_PRISM_MODEL | cv2.CALIB_TILTED_MODEL,
   )
 
   def __str__(self):
